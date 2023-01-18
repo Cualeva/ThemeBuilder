06effde14cb9883ea6a7e4032724db72075fd368
CualeMarke <luigi.marchesi@cualeva.com>
2023-01-18 13:57:42 +0100

    fix

--- src/app/promise-helper.ts
+++ src/app/promise-helper.ts
@@ -2,6 +2,5 @@ export const mutePromise = (promise: Promise<unknown>): void => {
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     if(promise && promise.catch) {
         promise.catch(null);
-        console.log(promsie)
     }
 };
