3acc45f4157528ce4e3240b5b85cf970b5860432
CualeMarke <luigi.marchesi@cualeva.com>
2025-02-24 09:30:42 +0100

    Upgrade DevExtreme license

--- src/app/global.ts
+++ src/app/global.ts
@@ -9,5 +9,5 @@ export const GlobalVariable = {
     devExpressUrl: 'https://js.devexpress.com/api/themebuilder',
     currentVariables: null,
     currentCss: null,
-    license: 'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImE0MTZhNzRiLTJiM2QtNGMwNi1hMDhmLWU0ZDU4OTE1YWNlNiIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjQxCn0=.V1EQ1L5okA2k06Op11nQO5tgCi/0yzvLs10hKw0U/FWnhVo97oiGjnL0XZxw7X5FLUMpU9tYohR4K4Ql7Cz8Y6Ay3PGcsejVuPuzpWp/CHZHxkbtXGdTM8uEIs8juL/YXoN0LA=='
+    license: 'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImE0MTZhNzRiLTJiM2QtNGMwNi1hMDhmLWU0ZDU4OTE1YWNlNiIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjQyCn0=.a+SSil+iN/qlh5odCcB2WLeI36GrMUJNLWPX3Ildzonu0zGobGrC3R7kxa61+nIlGfDYAIU0hDLy4ioamQtOZRfOooUC2jBJ0Raa4POXpfTRXfy946e+/8FZH5aHwcDMG4HLNA=='
 };
